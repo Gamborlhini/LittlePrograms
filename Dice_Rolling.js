2558c8c0d02edc39df94b9bd3d98ec67cf00efd8
NikhilSubhas <nikhil.subhas@gmail.com>
2018-08-19 15:33:28 -0400

    Add files via upload

---
+++ Dice_Rolling.js
@@ -0,0 +1,31 @@
+function rollad100() {
+  alert("You got " + Math.ceil(Math.random()*100))
+}
+function rollad20() {
+alert("You got " + Math.ceil(Math.random()*20));
+}
+function rollad12() {
+  alert("You got " + Math.ceil(Math.random()*12));
+}
+function rollad10() {
+  alert("You got " + Math.ceil(Math.random()*10));
+}
+function rollad8() {
+  alert("You got " + Math.ceil(Math.random()*8));
+}
+function rollad6() {
+  alert("You got " + Math.ceil(Math.random()*6));
+}
+function rollad4() {
+  alert("You got " + Math.ceil(Math.random()*4));
+}
+function flipaCoin() {
+  var random = Math.random();
+  if (Math.random() > .5) {
+    alert("You got heads!");
+}
+  else {
+    alert("You got tails!");
+  }
+  console.log(random);
+}
---
+++ Password_Possible_Combos.js
@@ -0,0 +1,134 @@
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*","(", ")", "!", "@", "#", "$", "%", "^", "&", "*", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
+"!", "@", "#", "$", "%", "^", "&", "*","(", ")", "!", "@", "#", "$", "%", "^", "&", "*", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")","_","_","_","_","_","_","_","_","_","_"
+];
+var output = document.getElementById('output');
+var pi = 3.1415926535897932384626433832795028841971;
+var InputNumbers = document.getElementById('Numbers');
+var InputLetters = document.getElementById('Letters');
+var CapitalcharactersAthroughR = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R"];
+var CapitalCharactersSthroughZ = ["S", "T", "U", "V", "W", "X", "Y", "Z"];
+var LowerCaseCharactersAthroughR = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"];
+var LowerCaseCharactersSthroughZ = ["s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9];
+var numpassword = numbers;
+var numbersandSpecialChars = numbers.concat(specialCharacters);
+var lettersPasscode = CapitalcharactersAthroughR.concat(CapitalCharactersSthroughZ, LowerCaseCharactersAthroughR, LowerCaseCharactersSthroughZ);
+var alphanumericpassword = CapitalcharactersAthroughR.concat(CapitalCharactersSthroughZ, LowerCaseCharactersAthroughR, LowerCaseCharactersSthroughZ, numbers);
+var allpasscode = CapitalcharactersAthroughR.concat(CapitalCharactersSthroughZ, LowerCaseCharactersAthroughR, LowerCaseCharactersSthroughZ, numbers, specialCharacters);
+var lettersandSpecialCharactersPasscode = CapitalcharactersAthroughR.concat(CapitalCharactersSthroughZ, LowerCaseCharactersAthroughR, LowerCaseCharactersSthroughZ, specialCharacters);
+function alphaNumericPasswordGenerator12Digit() {
+  var alphaNumericPassword = [];
+  var length = document.getElementById('Length').value;
+  if (length == 0) {
+}
+  else {
+    for (var j = 0; j < length; j++) {
+      alphaNumericPassword.push(alphanumericpassword[Math.floor(Math.random()*72)]);
+    }
+    document.getElementById('output').innerHTML = alphaNumericPassword.join("");
+  }
+}
+function possibleCombos() {
+  var amountofDigits = Number (prompt ("How many digits are there in your password?"));
+  var numbersPerDigit = Number (prompt ("How many different characters/numbers can each digit be?"));
+  var pow = Math.pow(numbersPerDigit, amountofDigits);
+  alert ("There are " + pow + " possible combinations for your password.");
+}
+function dynamicPassword() {
+  var UltimatePassword = [];
+  var length = document.getElementById('Length').value;
+  if (length == 0) {
+  }
+  else {
+    for (var i = 0; i < length; i++) {
+      UltimatePassword.push(allpasscode[Math.floor(Math.random()*148)]);
+    }
+    document.getElementById('output').innerHTML = UltimatePassword.join("");
+  }
+}
+function numbersOnly() {
+  var numbersArray = [];
+  var length = document.getElementById('Length').value;
+  if (length == 0) {
+  }
+  else {
+    for (var k = 0; k < length; k++) {
+      numbersArray.push(numbers[Math.floor(Math.random()*20)]);
+    }
+    document.getElementById('output').innerHTML = numbersArray.join("");
+  }
+}
+function lettersOnly() {
+  var lettersArray = [];
+  var length = document.getElementById('Length').value;
+  if (length == 0) {
+  }
+  else {
+    for (var l = 0; l < length; l++) {
+      lettersArray.push(lettersPasscode[Math.floor(Math.random()*52)]);
+    }
+    document.getElementById('output').innerHTML = lettersArray.join("");
+  }
+}
+function specialCharactersOnly() {
+  var specialCharactersArray = [];
+  var length = document.getElementById('Length').value;
+  if (length == 0) {
+  }
+  else {
+    for (var m = 0; m < length; m++) {
+      specialCharactersArray.push(specialCharacters[Math.floor(Math.random()*66)]);
+    }
+    document.getElementById('output').innerHTML = specialCharactersArray.join("");
+  }
+}
+function numbersandSpecialCharacters() {
+  var numbersandSpecialCharactersArray = [];
+  var length = document.getElementById('Length').value;
+  if (length == 0) {
+  }
+  else {
+    for (var n = 0; n < length; n++) {
+      numbersandSpecialCharactersArray.push(numbersandSpecialChars[Math.floor(Math.random()*76)]);
+    }
+    document.getElementById('output').innerHTML = numbersandSpecialCharactersArray.join("");
+  }
+}
+function lettersandSpecialCharacters() {
+  var lettersandSpecialCharactersArray = [];
+  var length = document.getElementById('Length').value;
+  if (length == 0) {
+  }
+  else {
+    for (var o = 0; o < length; o++) {
+      lettersandSpecialCharactersArray.push(lettersandSpecialCharactersPasscode[Math.floor(Math.random()*118)]);
+    }
+    document.getElementById('output').innerHTML = lettersandSpecialCharactersArray.join("");
+  }
+}
+function frn() {
+  if (document.getElementById('Numbers').checked && document.getElementById('Letters').checked && document.getElementById('Special_Characters').checked == false) {
+    alphaNumericPasswordGenerator12Digit();
+  }
+
+  else if (document.getElementById('Numbers').checked && document.getElementById('Special_Characters').checked && document.getElementById('Letters').checked) {
+    dynamicPassword();
+  }
+  else if (document.getElementById('Numbers').checked && document.getElementById('Special_Characters').checked == false && document.getElementById('Letters').checked == false) {
+    numbersOnly();
+  }
+  else if (document.getElementById('Numbers').checked == false && document.getElementById('Special_Characters').checked == false && document.getElementById('Letters').checked) {
+    lettersOnly();
+  }
+  else if (document.getElementById('Numbers').checked == false && document.getElementById('Special_Characters').checked && document.getElementById('Letters').checked == false) {
+    specialCharactersOnly();
+  }
+  else if (document.getElementById('Numbers').checked && document.getElementById('Special_Characters').checked && document.getElementById('Letters').checked == false) {
+    numbersandSpecialCharacters();
+  }
+  else if (document.getElementById('Numbers').checked == false && document.getElementById('Special_Characters').checked && document.getElementById('Letters').checked) {
+    lettersandSpecialCharacters();
+  }
+  else {
+  }
+}
