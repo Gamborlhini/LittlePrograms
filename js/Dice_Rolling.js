e165c83c304635d60eba9f67a2ea71977a80a459
Gamborlhini <nikhil.subhas@gmail.com>
2018-12-27 19:37:40 -0500

    Clean Up

--- js/Dice_Rolling.js
+++ js/Dice_Rolling.js
@@ -1,9 +1,10 @@
 var d100, d20, d12, d10, d8, d6, d4;
-//D100
+//For p5.js
 function setup() {
 }
 function rollaBlank(bottom,top) {
   alert("You got " + int(random(bottom, top)));
 }
+//Also p5.js
 function draw() {
 }
